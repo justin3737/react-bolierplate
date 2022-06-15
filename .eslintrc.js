1c76d3d84072d3566ca4df76f440fd8df5899318
Justin Wu <juns3700@gmail.com>
2022-06-15 11:53:39 +0800

    eslint: update indent

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,27 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
+  "env": {
+    "browser": true,
+    "es2021": true
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+  ],
+  "parser": "@typescript-eslint/parser",
+  "parserOptions": {
+    "ecmaFeatures": {
+      "jsx": true,
     },
-    "extends": "plugin:react/recommended",
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": "latest",
-        "sourceType": "module"
+    "ecmaVersion": 12,
+    "sourceType": 'module',
+  },
+  "plugins": ['react', 'react-hooks', "@typescript-eslint"],
+  "rules": {
+    "indent": ["error", 2]
+  },
+  "settings": {
+    "react": {
+      "version": 'detect',
     },
-    "plugins": [
-        "react",
-        "@typescript-eslint"
-    ],
-    "rules": {
-    }
+  },
 }
