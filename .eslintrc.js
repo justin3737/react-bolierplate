9528a139a7f0c7595d69cb7b84e9ebb33f97224c
Justin Wu <juns3700@gmail.com>
2022-06-15 11:01:45 +0800

    add TS elint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,21 @@
-/* .eslintrc.js */
 module.exports = {
-  "extends": "airbnb",
-  "plugins": [
-      "react"
-  ],
-  "parser": "babel-eslint",
-  "rules": {
-    "strict": 0
-  },
-};
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": "plugin:react/recommended",
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react",
+        "@typescript-eslint"
+    ],
+    "rules": {
+    }
+}
